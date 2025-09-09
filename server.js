c2dd3ef2c3135847546e20c16dfbaf2160ece7db
saswatjit ptra <saswatp813@gmail.com>
2025-09-09 18:57:14 +0530

    -fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://foody-gram-f.vercel.app/",
+    origin: "https://foody-gram-f.vercel.app",
     credentials: true,
   })
 );
